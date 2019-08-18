4ed0cea2040820271a867a0f9dee15e9ce47e917
Luiz Otávio Miranda <todoespacoonline@gmail.com>
2019-08-18 11:19:06 -0300

    Renderizando o carrinho - Parte 1

--- produto/templatetags/omfilters.py
+++ produto/templatetags/omfilters.py
@@ -8,3 +8,8 @@ register = Library()
 @register.filter
 def formata_preco(val):
     return utils.formata_preco(val)
+
+
+@register.filter
+def cart_total_qtd(carrinho):
+    return utils.cart_total_qtd(carrinho)
--- utils/utils.py
+++ utils/utils.py
@@ -1,2 +1,6 @@
 def formata_preco(val):
     return f'R$ {val:.2f}'.replace('.', ',')
+
+
+def cart_total_qtd(carrinho):
+    return sum([item['quantidade'] for item in carrinho.values()])
