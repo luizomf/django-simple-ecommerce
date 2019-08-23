7637cbb536691d36901228d0bc64b0042c0329c8
Luiz Otávio Miranda <luizomf@gmail.com>
2019-08-23 12:51:36 -0300

    Adicionado validação de CPF único.

--- perfil/models.py
+++ perfil/models.py
@@ -58,6 +58,16 @@ class Perfil(models.Model):
     def clean(self):
         error_messages = {}
 
+        cpf_enviado = self.cpf or None
+        cpf_salvo = None
+        perfil = Perfil.objects.filter(cpf=cpf_enviado).first()
+
+        if perfil:
+            cpf_salvo = perfil.cpf
+
+            if cpf_salvo is not None and self.pk != perfil.pk:
+                error_messages['cpf'] = 'CPF já existe.'
+
         if not valida_cpf(self.cpf):
             error_messages['cpf'] = 'Digite um CPF válido'
 
